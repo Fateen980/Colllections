eddd7b18bd491cd2aa4a5c3a0871dcab9559a4df
fateen khaled <fateen khaled>
2017-07-26 19:27:18 +0300

    first commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Collections</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="{!! asset('css/normalize.css')!!}">
+    <link rel="stylesheet" href="{!! asset('css/font-awesome.css')!!}">
+    <link rel="stylesheet" href="{!! asset('css/bootstrap.min.css')!!}">
+    <link rel="stylesheet" href="{!! asset('css/templatemo-style.css')!!}">
+    <script src="{!! asset('js/vendor/modernizr-2.6.2.min.js')!!}"></script>
+
+
+    <title>Collections</title>
+
+</head>
+
+<body>
+
+<!--[if lt IE 7]>
+<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<div id="loader-wrapper">
+    <div id="loader"></div>
+</div>
+
+<div class="content-bg"></div>
+<div class="bg-overlay"></div>
+
+<!-- SITE TOP -->
+<div class="site-top">
+    <div class="site-header clearfix">
+        <div class="container">
+            <a href="#" class="site-brand pull-left"><strong>Collections</strong></a>
+            <div class="social-icons pull-right">
+                <ul>
+                    <li><a href="#" class="fa fa-facebook"></a></li>
+                    <li><a href="#" class="fa fa-twitter"></a></li>
+                    <li><a href="#" class="fa fa-behance"></a></li>
+                    <li><a href="#" class="fa fa-dribbble"></a></li>
+                    <li><a href="#" class="fa fa-google-plus"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div> <!-- .site-header -->
+    <div class="site-banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-offset-2 col-md-8 text-center">
+                    <h2>Welcome To  <span class="blue">Collections</span><span class="green"></span></h2>
+                    <p><span class="blue">Collections </span><span class="green"></span>is ...</p>
+                </div>
+            </div>
+            <div class="row">
+                <form action="#" method="post" class="subscribe-form">
+                    <fieldset class="col-md-offset-4 col-md-3 col-sm-8">
+                        <input type="email" id="subscribe-email" placeholder="Enter your email...">
+                    </fieldset>
+                    <fieldset class="col-md-5 col-sm-4">
+                        <input type="submit" id="subscribe-submit" class="button white" value="Subscribe!">
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div> <!-- .site-banner -->
+</div> <!-- .site-top -->
+
+
